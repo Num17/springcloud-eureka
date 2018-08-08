bbf28b9d77fe34a3010c07881c02e913ba0c0523
17 <2540705109@qq.com>
2018-08-08 23:58:48 +0800

    test

--- eureka-client-ribbon/src/main/java/com/kayakwise/eurekaclientribbon/EurekaClientRibbonApplication.java
+++ eureka-client-ribbon/src/main/java/com/kayakwise/eurekaclientribbon/EurekaClientRibbonApplication.java
@@ -11,12 +11,6 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class EurekaClientRibbonApplication {
 
-    @Bean
-    @LoadBalanced
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(EurekaClientRibbonApplication.class, args);
     }
